b078edb3901b17abad2dab8c46f34b466724608e
Ratanax <hugoromerogui@gmail.com>
2024-06-27 16:20:07 -0400

    aaaaaaaaa crispy instalado

--- PlayNexxus/settings.py
+++ PlayNexxus/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'crispy_forms',
+    'crispy_bootstrap4',
+   
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL= "home"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
